b339a758b3aa0db31412c81e321f8415f6bf7409
Evgenia-bit <ev.mo4alina@yandex.ru>
2023-10-18 12:02:11 +0300

    feat: init

--- lib/features/example/domain/use_case.dart
+++ lib/features/example/domain/use_case.dart
@@ -1,7 +1,9 @@
+// ignore: one_member_abstracts
 abstract interface class DecrementCounterUseCase {
   int decrement();
 }
 
+// ignore: one_member_abstracts
 abstract interface class IncrementCounterUseCase {
   int increment();
 }
