e8e83056182f17f45fb4bc7ae7b47416b152f835
Evgenia-bit <ev.mo4alina@yandex.ru>
2023-10-24 16:54:50 +0300

    chore: updated text theme

--- packages/ui_kit/lib/src/styles/text/text_extention.dart
+++ packages/ui_kit/lib/src/styles/text/text_extention.dart
@@ -3,41 +3,41 @@ import 'package:ui_kit/src/styles/text/text_style.dart';
 
 /// App text style scheme.
 class AppTextTheme extends ThemeExtension<AppTextTheme> {
-  /// Text style 14_140.
+  /// Text style 14.
   final TextStyle regular14;
 
-  /// Text style 16_124.
+  /// Text style 16.
   final TextStyle regular16;
 
-  /// Text style 14_140_500.
-  final TextStyle medium14;
-
-  /// Text style 16_124_500.
-  final TextStyle medium16;
-
-  /// Text style 14_140_700.
-  final TextStyle bold14;
-
-  /// Text style 16_124_700.
+  /// Text style 16_700.
   final TextStyle bold16;
+
+  /// Text style 18_700.
+  final TextStyle bold18;
+
+  /// Text style 24_700.
+  final TextStyle bold24;
+
+  /// Text style 30_700.
+  final TextStyle bold30;
 
   AppTextTheme._({
     required this.regular14,
     required this.regular16,
-    required this.medium14,
-    required this.medium16,
-    required this.bold14,
     required this.bold16,
+    required this.bold18,
+    required this.bold24,
+    required this.bold30,
   });
 
   /// Base app text theme.
   AppTextTheme.base()
       : regular14 = AppTextStyle.regular14.value,
         regular16 = AppTextStyle.regular16.value,
-        medium14 = AppTextStyle.medium14.value,
-        medium16 = AppTextStyle.medium16.value,
-        bold14 = AppTextStyle.bold14.value,
-        bold16 = AppTextStyle.bold16.value;
+        bold16 = AppTextStyle.bold16.value,
+        bold18 = AppTextStyle.bold18.value,
+        bold24 = AppTextStyle.bold24.value,
+        bold30 = AppTextStyle.bold30.value;
 
   @override
   ThemeExtension<AppTextTheme> lerp(
@@ -51,10 +51,10 @@ class AppTextTheme extends ThemeExtension<AppTextTheme> {
     return AppTextTheme._(
       regular14: TextStyle.lerp(regular14, other.regular14, t)!,
       regular16: TextStyle.lerp(regular16, other.regular16, t)!,
-      medium14: TextStyle.lerp(medium14, other.medium14, t)!,
-      medium16: TextStyle.lerp(medium16, other.medium16, t)!,
-      bold14: TextStyle.lerp(bold14, other.bold14, t)!,
       bold16: TextStyle.lerp(bold16, other.bold16, t)!,
+      bold18: TextStyle.lerp(bold18, other.bold18, t)!,
+      bold24: TextStyle.lerp(bold24, other.bold24, t)!,
+      bold30: TextStyle.lerp(bold30, other.bold30, t)!,
     );
   }
 
@@ -69,18 +69,18 @@ class AppTextTheme extends ThemeExtension<AppTextTheme> {
   ThemeExtension<AppTextTheme> copyWith({
     TextStyle? regular14,
     TextStyle? regular16,
-    TextStyle? medium14,
-    TextStyle? medium16,
-    TextStyle? bold14,
     TextStyle? bold16,
+    TextStyle? bold18,
+    TextStyle? bold24,
+    TextStyle? bold30,
   }) {
     return AppTextTheme._(
       regular14: regular14 ?? this.regular14,
       regular16: regular16 ?? this.regular16,
-      medium14: medium14 ?? this.medium14,
-      medium16: medium16 ?? this.medium16,
-      bold14: bold14 ?? this.bold14,
       bold16: bold16 ?? this.bold16,
+      bold18: bold18 ?? this.bold18,
+      bold24: bold24 ?? this.bold24,
+      bold30: bold30 ?? this.bold30,
     );
   }
 }
