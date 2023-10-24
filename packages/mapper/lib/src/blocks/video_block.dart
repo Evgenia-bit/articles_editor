6658228f0f06af89877c225e3f6d12a2815fa570
Evgenia-bit <ev.mo4alina@yandex.ru>
2023-10-24 19:30:37 +0300

    chore: string moved to constant

--- packages/mapper/lib/src/blocks/video_block.dart
+++ packages/mapper/lib/src/blocks/video_block.dart
@@ -3,6 +3,8 @@ import 'package:mapper/src/blocks/block.dart';
 import 'package:mapper/src/models/video_block_model.dart';
 import 'package:mapper/src/widgets/video.dart';
 
+const _noVideoAssetName = 'packages/mapper/assets/no_video.png';
+
 class VideoBlock extends Block<VideoBlockModel> {
   VideoBlock(super.model);
 
@@ -10,10 +12,7 @@ class VideoBlock extends Block<VideoBlockModel> {
   Widget buildWidget(BuildContext context) {
     final uri = Uri.tryParse(model.url);
     if (uri == null) {
-      return Image.asset(
-        'packages/mapper/assets/no_video.png',
-        width: 250,
-      );
+      return Image.asset(_noVideoAssetName, width: 250);
     }
     return Video(uri: uri);
   }
