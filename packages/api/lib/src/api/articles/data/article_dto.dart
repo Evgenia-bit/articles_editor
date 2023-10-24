c0fff373a70ce396d8ec6387d190e7e0347b9aa9
Evgenia-bit <ev.mo4alina@yandex.ru>
2023-10-24 12:51:28 +0300

    fix: fixed api module

--- packages/api/lib/src/api/articles/data/article_dto.dart
+++ packages/api/lib/src/api/articles/data/article_dto.dart
@@ -4,13 +4,13 @@ class ArticleDto {
   final int id;
   final String? title;
   final String? description;
-  final String? imageURL;
+  final String? imageUrl;
   final List<Map<String, dynamic>> blocks;
 
-  ArticleDto.fromJSON(Map<String, dynamic> json)
+  ArticleDto.fromJson(Map<String, dynamic> json)
       : id = json['id'] as int,
         title = safeCast<String>(json['title']),
         description = safeCast<String>(json['description']),
-        imageURL = safeCast<String>(json['image_url']),
+        imageUrl = safeCast<String>(json['image_url']),
         blocks = (json['blocks'] as List).cast();
 }
