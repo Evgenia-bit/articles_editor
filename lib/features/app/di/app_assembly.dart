e66b531fa6408f2bde77c17f9c835a9374a4a333
Evgenia-bit <ev.mo4alina@yandex.ru>
2023-11-01 11:55:48 +0300

    feat: added DI

---
+++ lib/features/app/di/app_assembly.dart
@@ -0,0 +1,30 @@
+import 'package:api/api.dart';
+import 'package:artus/features/app/di/i_app_assembly.dart';
+import 'package:artus_logger/logger.dart';
+import 'package:mapper/mapper.dart';
+
+class AppAssembly implements IAppAssembly {
+  late final ArticlesApi _articlesApi;
+  late final BlockParser _blockParser;
+  late final Mapper _mapper;
+  late final LogWriter _logger; 
+
+  @override
+  ArticlesApi get articlesApi => _articlesApi;
+
+  @override
+  BlockParser get blockParser => _blockParser;
+
+  @override
+  Mapper get mapper => _mapper;
+
+  @override
+  LogWriter get logger => _logger;
+
+  AppAssembly() {
+    _articlesApi = ArticlesApiStub();
+    _blockParser = BlockParser();
+    _mapper = Mapper(blockParser: _blockParser);
+    _logger = Logger.withStrategies({SimpleLogStrategy()});
+  }
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,35 +1,8 @@
-import 'package:artus/features/example/data/repository.dart';
-import 'package:artus/features/example/presentation/widget_component.dart';
-import 'package:flutter/material.dart';
-import 'package:localizations/localizations.dart';
-import 'package:ui_kit/ui_kit.dart';
+import 'package:artus/features/app/app.dart';
+import 'package:artus/features/app/di/app_assembly.dart';
+import 'package:flutter/widgets.dart';
 
 void main() {
-  runApp(const MyApp());
-}
-
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
-  @override
-  Widget build(BuildContext context) {
-    final repository = CounterRepository(counter: 0);
-
-    return MaterialApp(
-      localizationsDelegates: const [
-        AppLocalizations.delegate,
-        GlobalMaterialLocalizations.delegate,
-        GlobalWidgetsLocalizations.delegate,
-        GlobalCupertinoLocalizations.delegate,
-      ],
-      supportedLocales: AppLocalizations.supportedLocales,
-      title: 'Counter',
-      theme: AppThemeData.lightTheme,
-      darkTheme: AppThemeData.darkTheme,
-      home: CounterWidgetComponent(
-        decrementCounterUseCase: repository,
-        incrementCounterUseCase: repository,
-        getCounterUseCase: repository,
-      ),
-    );
-  }
+  final appAssembly = AppAssembly();
+  runApp(MyApp(appAssembly: appAssembly));
 }
