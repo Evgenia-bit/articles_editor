7b7de475f673448a26ca88f146b6da32542040c3
Evgenia-bit <ev.mo4alina@yandex.ru>
2023-11-02 10:17:00 +0300

    chore: update FailureDisplayer

--- packages/core/lib/src/failure_displayer.dart
+++ packages/core/lib/src/failure_displayer.dart
@@ -13,7 +13,6 @@ class FailureDisplayer {
 
     if (failure is Failure) {
       return _displayFailure(context, failure);
-      return _displayFailure(context, failure);
     }
 
     await _messageController.show(context, l10n.failureUnknown);
@@ -21,18 +20,18 @@ class FailureDisplayer {
 
   Future<void> _displayFailure(BuildContext context, Failure failure) async {
     final message = failure.message;
-    if (message != null) return messageController.show(context, message);
+    if (message != null) return _messageController.show(context, message);
 
     final l10n = AppLocalizations.of(context)!;
 
     if (failure is InvalidTitle) {
-      return messageController.show(context, l10n.failureInvalidTitle);
+      return _messageController.show(context, l10n.failureInvalidTitle);
     }
 
     if (failure is ArticleNotFoundFailure) {
-      return messageController.show(context, l10n.failureArticleNotFound);
+      return _messageController.show(context, l10n.failureArticleNotFound);
     }
 
-    await messageController.show(context, l10n.failureUnknown);
+    await _messageController.show(context, l10n.failureUnknown);
   }
 }
